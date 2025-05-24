cc0949e55029c2aa5f158cb20771e2d323d3f588
lcosta-engqua <1406802@sga.pucminas.br>
2025-05-24 09:37:24 -0300

    impressão dos tipos de dados básicos no php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "Hello wold";
+echo "Hello Wold"; // strings - valores de texto
+
+echo "<br>";
+
+echo 2025;  // int - números inteiros
+echo "<br>";
+echo 10.57; // float - números com casas decimais
+
+echo "<br>";
+echo true; // bool - verdadeiro e falso
+var_dump (true); // bool - verdadeiro e falso
+echo "<br>";
+
+var_dump (null); // null - sem valor
+echo "<br>";
+var_dump ([
+    "João", 
+    2025, 
+    0
+]); // array - multiplos valores
